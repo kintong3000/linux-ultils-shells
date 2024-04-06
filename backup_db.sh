3faf3e3d850460f0cda677ed6aef0d4ac6fa4b66
Kintong Cheung <cheungkintong3000@gmail.com>
2024-04-06 23:06:38 +0800

    Add files via upload

---
+++ backup_db.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# 定义 Docker 容器名称和数据库认证信息
+CONTAINER_NAME="mysql_container"
+DB_USER="your_username"
+DB_PASSWORD="your_password"
+
+# 定义备份文件的存储位置
+BACKUP_DIR="/backup/db"
+
+# 创建存储备份的目录（如果它不存在的话）
+mkdir -p "$BACKUP_DIR"
+
+# 定义备份文件的名称格式
+DATE=$(date +%Y%m%d_%H%M%S)
+BACKUP_FILE="$BACKUP_DIR/mydb_$DATE.sql"
+
+# 使用 docker exec 执行 mysqldump
+docker exec $CONTAINER_NAME mysqldump -u $DB_USER -p$DB_PASSWORD --all-databases > $BACKUP_FILE
+
+
+
+echo "Database backup has been created successfully in the Docker container."
